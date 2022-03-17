c1c5079472256832401b2192ae146b5c260390c6
Davi Leal Marçal <davilealmarcal198@gmail.com>
2022-03-17 13:16:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 let num1 = 8
 let num2 = 2
-let feet = document.getElementById("feet")
-let inptNumber = document.getElementById("inpt-number").value
+let sumEl = document.getElementById("sum-el")
 let result = 0
-
-// Conversion functions
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
 
 function add() {
-    result = inptNumber * 3.2808 
-    feet.textContent = result
+    result = num1 + num2
+    sumEl.textContent = "Sum: " + result
 }
 
 function subtract() {
@@ -25,10 +24,3 @@ function multiply() {
     result = num1 * num2
     sumEl.textContent = "Sum: " + result
 }
-
-
-// E.g. if the user clicks on the "Plus" button, you should render
-// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
-
-
-
